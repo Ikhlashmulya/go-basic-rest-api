d57ea683ad109825eb203295ac612ec28fa60855
Ikhlash Mulyanurahman <nurahmanmulya@gmail.com>
2023-03-14 21:13:55 +0700

    delete first fake data

--- database/database.go
+++ database/database.go
@@ -5,12 +5,6 @@ import "github.com/ikhlashmulya/go-basic-rest-api/model"
 var database = make(map[string]model.Student)
 
 func FindAll() []model.Student {
-	database["01"] = model.Student{
-		Id:    "01",
-		Name:  "Ikhlash Mulyanurahman",
-		Email: "nurahmanmulya@gmail.com",
-	}
-
 	items := make([]model.Student, 0)
 	for _, val := range database {
 		items = append(items, val)
