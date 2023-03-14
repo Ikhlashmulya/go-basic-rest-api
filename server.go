2a8b3f40236424bbd7bb2d2910924148638ea931
Ikhlash Mulyanurahman <nurahmanmulya@gmail.com>
2023-03-14 17:22:58 +0700

    create router and add handler get students

--- server.go
+++ server.go
@@ -3,13 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/ikhlashmulya/go-basic-rest-api/handler"
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
+
+	//router
+	router := httprouter.New()
+	router.GET("/students", handler.GetStudents)
+
+	//server
 	server := http.Server{
-		Addr: ":8000",
+		Addr:    ":8080",
+		Handler: router,
 	}
 
+	log.Println("Server Running on 8080")
 	err := server.ListenAndServe()
 	log.Fatal(err)
 }
